1539a00209dce544c28d4a145c3f57a2a969f2d6
Andrei <wwandyww@yandex.ru>
2020-08-26 03:03:59 +0500

    Added game controls
    Added game-controls with settings and toggle button
    Added button component

---
+++ src/components/button/button.ts
@@ -0,0 +1,13 @@
+import './button.scss';
+
+export class Button {
+  private _node: HTMLButtonElement;
+
+  constructor(node: HTMLButtonElement) {
+    this._node = node;
+  }
+
+  onPress(callback: () => void): void {
+    this._node.addEventListener('click', callback);
+  }
+}
---
+++ src/components/game-controls/game-controls.ts
@@ -0,0 +1,29 @@
+import './game-controls.scss';
+import { Button } from '../button/button';
+
+class GameControls {
+  private _node: HTMLElement;
+  private _mainButton: Button;
+  private _settingsButton: Button;
+
+  constructor(node: HTMLElement) {
+    this._node = node;
+    const mainButtonNode: HTMLButtonElement = node
+      .querySelector('.game-controls__main-button')
+      .querySelector('.button');
+    const settingsButtonNode: HTMLButtonElement = node
+      .querySelector('.game-controls__settings-button')
+      .querySelector('.button');
+
+    this._mainButton = new Button(mainButtonNode);
+    this._settingsButton = new Button(settingsButtonNode);
+  }
+
+  onMainButtonPress(callback: () => void): void {
+    this._mainButton.onPress(callback);
+  }
+
+  onSettingsButtonPress(callback: () => void): void {
+    this._settingsButton.onPress(callback);
+  }
+}
