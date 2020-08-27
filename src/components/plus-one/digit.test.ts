8128b89b93eae151133bee0cc86da6d1f8574cc3
Andrei <wwandyww@yandex.ru>
2020-08-28 04:20:11 +0500

    Added exception if render called second time

---
+++ src/components/plus-one/digit.test.ts
@@ -0,0 +1,73 @@
+import { expect } from 'chai';
+import { JSDOM } from 'jsdom';
+import { Digit } from './digit';
+
+const dom = new JSDOM('<html><body id="root"></body></html>');
+global.document = dom.window.document;
+
+const requiredClasses = ['plus-one__digit', 'js-plus-one__digit'];
+const rootNode: HTMLElement = document.querySelector('#root');
+
+let digit: Digit;
+beforeEach(() => {
+  digit = new Digit(rootNode);
+});
+
+afterEach(() => {
+  document.body.innerHTML = '';
+});
+
+describe('Digit', () => {
+  describe('render', () => {
+    it('Renders html input element to the DOM', () => {
+      digit.render();
+
+      expect(document.querySelectorAll('input').length).to.equal(1);
+    });
+
+    it('Rendered element has required classes', () => {
+      digit.render();
+
+      const node = document.querySelector('input');
+
+      requiredClasses.forEach((className) => expect(node.classList.contains(className)).to.equal(true));
+    });
+
+    it('Throws and exception if called second time', () => {
+      digit.render();
+
+      const func = digit.render.bind(digit);
+      expect(func).to.throw('Element is already rendered. Destroy current instance by calling destroy first.');
+    });
+  });
+
+  describe('showCorrectDigit', () => {
+    const correctDigit = 8;
+
+    it('Shows correct digit', () => {
+      showDigit();
+
+      const node = document.querySelector('input');
+      expect(+node.value).to.equal(correctDigit);
+    });
+
+    it('Shown value is readonly', () => {
+      showDigit();
+
+      const node = document.querySelector('input');
+      expect(node.readOnly).to.equal(true);
+    });
+
+    it("Throws an exception if element wasn't rendered before", () => {
+      const func = digit.showCorrectDigit.bind(digit);
+      expect(func).to.throw("Element wasn't rendered before. Please, call 'render' method first.");
+    });
+
+    function showDigit(): void {
+      digit.render();
+
+      digit.setCorrectDigit(correctDigit);
+      digit.showCorrectDigit();
+    }
+  });
+});
--- src/components/plus-one/digit.ts
+++ src/components/plus-one/digit.ts
@@ -1,17 +1,39 @@
-class Digit {
+export class Digit {
   private _parentNode: HTMLElement;
   private _node: HTMLInputElement;
+  private _correctDigit: number;
 
   constructor(parentNode: HTMLElement) {
     this._parentNode = parentNode;
+    this._node = null;
+    this._correctDigit = 0;
   }
 
   render() {
+    if (this._node !== null)
+      this._throwError('Element is already rendered. Destroy current instance by calling destroy first.');
+
     const classes = ['plus-one__digit', 'js-plus-one__digit'];
 
     this._node = document.createElement('input');
     this._node.classList.add(...classes);
 
     this._parentNode.appendChild(this._node);
+  }
+
+  setCorrectDigit(digit: number) {
+    this._correctDigit = digit;
+  }
+
+  showCorrectDigit() {
+    if (this._node === null) this._throwError("Element wasn't rendered before. Please, call 'render' method first.");
+
+    this._node.value = String(this._correctDigit);
+    this._node.readOnly = true;
+    this._node.classList.add('plus-one__digit_readonly');
+  }
+
+  _throwError(message: string): never {
+    throw new Error(message);
   }
 }
